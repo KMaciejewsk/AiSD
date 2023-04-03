f9977c106bc445c261484756aa314315dd4823ca
KMaciejewsk <115485162+KMaciejewsk@users.noreply.github.com>
2023-04-03 22:02:30 +0200

    Create AVL.py

---
+++ AVL.py
@@ -0,0 +1,189 @@
+class AVLNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.height = 1
+
+class AVLTree:
+    def __init__(self):
+        self.root = None
+
+    def height(self, node):
+        if node is None:
+            return 0
+        return node.height
+
+    def get_balance(self, node):
+        if node is None:
+            return 0
+        return self.height(node.left) - self.height(node.right)
+
+    def rotate_right(self, node):
+        left_child = node.left
+        right_child_of_left_child = left_child.right
+
+        left_child.right = node
+        node.left = right_child_of_left_child
+
+        node.height = 1 + max(self.height(node.left), self.height(node.right))
+        left_child.height = 1 + max(self.height(left_child.left), self.height(left_child.right))
+
+        return left_child
+
+    def rotate_left(self, node):
+        right_child = node.right
+        left_child_of_right_child = right_child.left
+
+        right_child.left = node
+        node.right = left_child_of_right_child
+
+        node.height = 1 + max(self.height(node.left), self.height(node.right))
+        right_child.height = 1 + max(self.height(right_child.left), self.height(right_child.right))
+
+        return right_child
+
+    def insert(self, key):
+        def _insert(node, key):
+            if node is None:
+                return AVLNode(key)
+
+            if key < node.key:
+                node.left = _insert(node.left, key)
+
+            else:
+                node.right = _insert(node.right, key)
+
+            node.height = 1 + max(self.height(node.left), self.height(node.right))
+
+            balance = self.get_balance(node)
+
+            if balance > 1 and key < node.left.key:
+                return self.rotate_right(node)
+
+            if balance < -1 and key > node.right.key:
+                return self.rotate_left(node)
+
+            if balance > 1 and key > node.left.key:
+                node.left = self.rotate_left(node.left)
+                return self.rotate_right(node)
+
+            if balance < -1 and key < node.right.key:
+                node.right = self.rotate_right(node.right)
+                return self.rotate_left(node)
+
+            return node
+
+        self.root = _insert(self.root, key)
+
+    def delete(self, key):
+        def _delete(node, key):
+            if node is None:
+                return node
+
+            if key < node.key:
+                node.left = _delete(node.left, key)
+
+            elif key > node.key:
+                node.right = _delete(node.right, key)
+
+            else:
+                if node.left is None and node.right is None:
+                    node = None
+
+                elif node.left is None:
+                    node = node.right
+
+                elif node.right is None:
+                    node = node.left
+
+                else:
+                    temp = self.find_min(node.right)
+
+                    node.key = temp.key
+
+                    node.right = _delete(node.right, temp.key)
+
+            if node is None:
+                return node
+
+            node.height = 1 + max(self.height(node.left), self.height(node.right))
+
+            balance = self.get_balance(node)
+
+            if balance > 1 and self.get_balance(node.left) >= 0:
+                return self.rotate_right(node)
+
+            if balance < -1 and self.get_balance(node.right) <= 0:
+                return self.rotate_left(node)
+
+            if balance > 1 and self.get_balance(node.left) < 0:
+                node.left = self.rotate_left(node.left)
+                return self.rotate_right(node)
+
+            if balance < -1 and self.get_balance(node.right) > 0:
+                node.right = self.rotate_right(node.right)
+                return self.rotate_left(node)
+
+            return node
+
+        self.root = _delete(self.root, key)
+
+    def find_min(self, node):
+        current = node
+        print(current.key)
+        while current.left is not None:
+            current = current.left
+            print(current.key)
+        return current
+
+    def find_max(self, node):
+        current = node
+        print(current.key)
+        while current.right is not None:
+            current = current.right
+            print(current.key)
+        return current
+
+    def display_inorder(self, node):
+        if node is not None:
+            self.display_inorder(node.left)
+            print(node.key, end=' ')
+            self.display_inorder(node.right)
+
+    def display_preorder(self, node):
+        if node is not None:
+            print(node.key, end=' ')
+            self.display_preorder(node.left)
+            self.display_preorder(node.right)
+
+    def display_subtree_preorder(self, key):
+        def _display(node, key):
+            if node is not None:
+                if node.key == key:
+                    print(node.key, end=' ')
+                    self.display_preorder(node.left)
+                    self.display_preorder(node.right)
+                else:
+                    _display(node.left, key)
+                    _display(node.right, key)
+
+        _display(self.root, key)
+
+    def display(self):
+        def _display(node, level=0):
+            if node is not None:
+                _display(node.right, level + 1)
+                print('   ' * level + '->', node.key, '[', node.height, ']')
+                _display(node.left, level + 1)
+
+        _display(self.root)
+
+avl_tree = AVLTree()
+avl_tree.insert(10)
+avl_tree.insert(20)
+avl_tree.insert(30)
+avl_tree.insert(40)
+avl_tree.insert(50)
+avl_tree.insert(25)
+avl_tree.display()
